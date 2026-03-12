852af36ff85458fb49d4e5edbe09549ec180cbb0
spolmag <spolmag@gmail.com>
2026-03-13 06:37:49 +0700

    add js file to shoHuay folder

---
+++ shohuayOnline/make_to_javascript.js
@@ -0,0 +1,27 @@
+let customer1 = [1, 'ลุงต่าย ซ.5', '123456789', 'spolmag', 'spolmag@gmail.com', '26 ม.5 ซ.5'];
+
+let staff1 = [3, 'อนุ เอกชัย', 'Sales'];
+
+let product1 = [{
+    productId : 8850545311114, 
+    category_1Id : 1, 
+    category_2Id : 1, 
+    productName : 'น้ำปลา ทิพรส ขวด 300มล,', 
+    unitPrice : 35.00
+}];
+
+let category1 = [{
+    category_1Id : 1,
+    category_1Name: 'เครื่องปรุงอาหาร'
+}];
+
+let category2 = [{
+    category_2Id : 1,
+    category_2Name : 'น้ำปลา, ซิอิ๊ว, เครื่องปรุงชนิดขวด',
+}];
+
+console.log(customer1);
+console.log(staff1);
+console.log(product1);
+console.log(category1);
+console.log(category2);
